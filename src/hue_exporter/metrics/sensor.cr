3294493283a637e43b1b6b153d77aa0ac41ad68c
Georg Gadinger <nilsding@nilsding.org>
2018-05-29 18:17:54 +0200

    Refactor value sanitisation

--- src/hue_exporter/metrics/sensor.cr
+++ src/hue_exporter/metrics/sensor.cr
@@ -16,42 +16,46 @@ module HueExporter
 
         hash["state"].each do |key, value|
           metric_name = [metric_name_base, "state_", key.as_s, metric_labels].join
-          metric_value : Int64 | Int32 | Float64 | Float32 = 0
 
-          # TODO: refactor this.
-          unless value.nil?
+          ok, metric_value = sanitise_value(value) do
+            # special handling for temperature to get a float
             if key == "temperature"
-              # special handling for temperature to get a float
-              metric_value = value.as_i64 / 100.0
-            elsif !value.as_bool?.nil?
-              metric_value = value.as_bool ? 1 : 0
-            elsif value.as_s?
-              next # no support for string values
-            else
-              metric_value = value.as_i64
+              value.as_i64 / 100.0
             end
           end
+          next unless ok
+
           metrics << [value.nil? ? "#" : "", metric_name, metric_value].join(" ").strip
         end
 
         hash["config"].each do |key, value|
           metric_name = [metric_name_base, "config_", key.as_s, metric_labels].join
-          metric_value : Int64 | Int32 | Float64 | Float32 = 0
-          unless value.nil?
-            if value.as_a?
-              next # no support for array values
-            elsif value.as_s?
-              next # no support for string values
-            elsif !value.as_bool?.nil?
-              metric_value = value.as_bool ? 1 : 0
-            else
-              metric_value = value.as_i64
-            end
-          end
+
+          ok, metric_value = sanitise_value(value)
+          next unless ok
+
           metrics << [value.nil? ? "#" : "", metric_name, metric_value].join(" ").strip
         end
 
         metrics
+      end
+
+      # overloading for blocks
+      private def sanitise_value(value) : Tuple(Bool, Int64 | Int32 | Float64 | Float32)
+        # block for special handling
+        block_value = yield value
+        return {true, block_value} if block_value
+        sanitise_value(value)
+      end
+
+      # sanitises the JSON::Any value to be used for prometheus
+      private def sanitise_value(value) : Tuple(Bool, Int64 | Int32 | Float64 | Float32)
+        # no support for nil, array, and string values
+        return {false, 0} if value.nil? || value.as_a? || value.as_s?
+        return {true, value.as_bool ? 1 : 0} if !value.as_bool?.nil?
+        return {true, value.as_f} if value.as_f?
+        return {true, value.as_f32} if value.as_f32?
+        {true, value.as_i64}
       end
     end
   end
