f897236e1ff6e07527f1aec6a3135676a6c6a559
Georg Gadinger <nilsding@nilsding.org>
2018-05-29 18:36:49 +0200

    Use compact_map instead of each, make union have less types

--- src/hue_exporter/metrics/sensor.cr
+++ src/hue_exporter/metrics/sensor.cr
@@ -14,48 +14,45 @@ module HueExporter
         metric_name_base = "hue_sensor_"
         metric_labels = "{device_type=#{hash["type"].as_s.inspect},name=#{hash["name"].as_s.inspect}}"
 
-        hash["state"].each do |key, value|
-          metric_name = [metric_name_base, "state_", key.as_s, metric_labels].join
+        metrics += hash["state"].as_h.compact_map do |key, value|
+          metric_name = [metric_name_base, "state_", key, metric_labels].join
 
           ok, metric_value = sanitise_value(value) do
             # special handling for temperature to get a float
-            if key == "temperature"
-              value.as_i64 / 100.0
+            if key == "temperature" && value.is_a?(Int64)
+              value / 100.0
             end
           end
           next unless ok
 
-          metrics << [value.nil? ? "#" : "", metric_name, metric_value].join(" ").strip
+          [value.nil? ? "#" : "", metric_name, metric_value].join(" ").strip
         end
 
-        hash["config"].each do |key, value|
-          metric_name = [metric_name_base, "config_", key.as_s, metric_labels].join
+        metrics += hash["config"].as_h.compact_map do |key, value|
+          metric_name = [metric_name_base, "config_", key, metric_labels].join
 
           ok, metric_value = sanitise_value(value)
           next unless ok
 
-          metrics << [value.nil? ? "#" : "", metric_name, metric_value].join(" ").strip
+          [value.nil? ? "#" : "", metric_name, metric_value].join(" ").strip
         end
 
         metrics
       end
 
       # overloading for blocks
-      private def sanitise_value(value) : Tuple(Bool, Int64 | Int32 | Float64 | Float32)
+      private def sanitise_value(value) : Tuple(Bool, Int64 | Float64)
         # block for special handling
         block_value = yield value
         return {true, block_value} if block_value
         sanitise_value(value)
       end
 
-      # sanitises the JSON::Any value to be used for prometheus
-      private def sanitise_value(value) : Tuple(Bool, Int64 | Int32 | Float64 | Float32)
-        # no support for nil, array, and string values
-        return {false, 0} if value.nil? || value.as_a? || value.as_s?
-        return {true, value.as_bool ? 1 : 0} if !value.as_bool?.nil?
-        return {true, value.as_f} if value.as_f?
-        return {true, value.as_f32} if value.as_f32?
-        {true, value.as_i64}
+      # sanitises the value to be used for prometheus
+      private def sanitise_value(value) : Tuple(Bool, Int64 | Float64)
+        return {true, value ? 1i64 : 0i64} if value.is_a?(Bool)
+        return {true, value} if value.is_a?(Float64 | Int64)
+        return {false, 0i64}
       end
     end
   end
